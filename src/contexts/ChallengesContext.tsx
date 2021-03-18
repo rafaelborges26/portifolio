6436ac928ac5bc4f266263c77f7152839665572b
= <rafael.borges2698@gmail.com>
2021-03-18 01:12:38 -0300

    added cookies

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useState, ReactNode, useEffect } from 'react'
+import Cookies from 'js-cookie'
 import challenges from '../../challenges.json'
 import { CompletedChallenges } from '../components/CompletedChallenges';
 
@@ -40,6 +41,15 @@ export function ChallengesProvider({ children }: ChallengeContextProps) {
         Notification.requestPermission()
     },[])
 
+
+    useEffect(() => {
+
+        Cookies.set('level', String(level))
+        Cookies.set('currentExperience', String(currentExperience))
+        Cookies.set('challengeCompleteded', String(challengeCompleteded))
+        
+
+    },[level, currentExperience, challengeCompleteded])
 
     const levelUp = () => {
         setLevel(level + 1);
